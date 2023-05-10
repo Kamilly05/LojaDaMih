f532f8e72d8ca938b95609c8c01e9a4c004f83bc
Kamilly05 <132912398+Kamilly05@users.noreply.github.com>
2023-05-10 10:45:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Olá, turma do AZ900";
-?>
+<!DOCTYPE
+<html>
+<head>
+<title>Minha pagina inicial</title>
+</head>
+<body>
+<h1>Minha pagina inicial</h1>
+<p>Bem vindo(a) à minha pagina inicial! Aqui você encontrará informações sobre mim e meus interesses.</p>
+<p>Sou uma aluna iniciante em programação e cloud computing!
+Gosto também  de assistir filmes,treinar e viajar.</p>
+<a href="#" class="button">Entre em contato</a>
+</body>                                                                                                                                                                                                   
+</html>
